80df52dc0f2269075bfe8531d166f2e5e4cfbef4
sinius15 <sinius15@gmail.com>
2014-08-02 15:27:21 +0200

    adding ant build file for automatic version controll 	new file:   build.properties 	new file:   builder.xml 	new file:   src/com/simpletasker/Lib.java

---
+++ src/com/simpletasker/Lib.java
@@ -0,0 +1,13 @@
+package com.simpletasker;
+
+public class Lib {
+
+	public static final String VERSION = "0.0.7";
+	
+	public static final String VERSION_MAJOR = "0";
+	public static final String VERSION_MINOR = "0";
+	public static final String BUILDNUMBER = "7";
+	
+	public static final String BUILD_TIME = "2014-08-02 15:24:46";
+	
+}
