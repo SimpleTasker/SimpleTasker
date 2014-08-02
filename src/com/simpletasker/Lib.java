bbd88be67239932a4a753c6178ace003fd1bf416
sinius15 <sinius15@gmail.com>
2014-08-02 15:30:18 +0200

    Adding javadoc to the Version variables in the Lib.java class. 	modified:   src/com/simpletasker/Lib.java

--- src/com/simpletasker/Lib.java
+++ src/com/simpletasker/Lib.java
@@ -2,12 +2,34 @@ package com.simpletasker;
 
 public class Lib {
 
+	/**
+	 * The current version of the program. This is autmoaticly changed every
+	 * build: so do not touch this variable!
+	 */
 	public static final String VERSION = "0.0.7";
-	
+
+	/**
+	 * The current major version of the program. This is autmoaticly changed
+	 * every build: so do not touch this variable!
+	 */
 	public static final String VERSION_MAJOR = "0";
+
+	/**
+	 * The current minor version of the program. This is autmoaticly changed
+	 * every build: so do not touch this variable!
+	 */
 	public static final String VERSION_MINOR = "0";
+
+	/**
+	 * The current buildnumber of the program. This is autmoaticly changed every
+	 * build: so do not touch this variable!
+	 */
 	public static final String BUILDNUMBER = "7";
-	
+
+	/**
+	 * The build time. This is autmoaticly changed every build: so do not touch
+	 * this variable!
+	 */
 	public static final String BUILD_TIME = "2014-08-02 15:24:46";
-	
+
 }
