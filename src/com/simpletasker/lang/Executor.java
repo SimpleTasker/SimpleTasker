4deb3222ae3ea18080cc66356f26a86a22176afc
sinius15 <sinius15@gmail.com>
2014-08-09 21:33:39 +0200

    test commit for double eddit

--- src/com/simpletasker/lang/Executor.java
+++ src/com/simpletasker/lang/Executor.java
@@ -1,5 +1,16 @@
 package com.simpletasker.lang;
 
+/**
+ * Super awsome class that does super cool stuff.
+ * @author Sinius
+ *
+ */
 public class Executor {
 
+	private String[] keyWords = {"start"};
+	
+	public String toString() {
+		return super.toString();
+	}
+	
 }
