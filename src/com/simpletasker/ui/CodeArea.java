875baacd10d79c962d5939ea1951016610f47244
sinius15 <sinius15@gmail.com>
2014-08-10 20:46:49 +0200

    laptop work 	modified:   src/com/simpletasker/ui/CodeArea.java

--- src/com/simpletasker/ui/CodeArea.java
+++ src/com/simpletasker/ui/CodeArea.java
@@ -1,9 +1,38 @@
 package com.simpletasker.ui;
 
 import javax.swing.JTextArea;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
-public class CodeArea extends JTextArea {
+public class CodeArea extends JTextArea implements CaretListener,  DocumentListener{
 
 	private static final long serialVersionUID = -7018375408410070748L;
 
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void caretUpdate(CaretEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
