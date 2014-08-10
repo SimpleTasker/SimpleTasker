82c8cfaca678f58bb34a16f3ae837e4f6272f2ac
sinius15 <sinius15@gmail.com>
2014-08-10 21:31:58 +0200

    Starting up creation of the CodePane: auto-completion, Line numbers 	deleted:    src/com/simpletasker/ui/CodeArea.java 	new file:   src/com/simpletasker/ui/CodePane.java 	new file:   src/com/simpletasker/ui/CodeTextArea.java 	modified:   src/com/simpletasker/ui/EditorFrame.java 	modified:   src/com/simpletasker/ui/GuiManager.java 	new file:   src/com/simpletasker/ui/SuggestionLabel.java

--- src/com/simpletasker/ui/CodeArea.java
+++
@@ -1,9 +0,0 @@
-package com.simpletasker.ui;
-
-import javax.swing.JTextArea;
-
-public class CodeArea extends JTextArea {
-
-	private static final long serialVersionUID = -7018375408410070748L;
-
-}
