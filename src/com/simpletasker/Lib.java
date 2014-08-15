3cbbfd0c08b1c0e837e737d19386563bdd6d146b
sinius15 <sinius15@gmail.com>
2014-08-15 18:17:46 +0200

    creating controls for the auto complete CodeArea.

--- src/com/simpletasker/Lib.java
+++ src/com/simpletasker/Lib.java
@@ -6,7 +6,7 @@ public class Lib {
 	 * The current version of the program. This is autmoaticly changed every
 	 * build: so do not touch this variable!
 	 */
-	public static final String VERSION = "0.1.2";
+	public static final String VERSION = "0.2.1";
 
 	/**
 	 * The current major version of the program. This is autmoaticly changed
@@ -18,18 +18,18 @@ public class Lib {
 	 * The current minor version of the program. This is autmoaticly changed
 	 * every build: so do not touch this variable!
 	 */
-	public static final String VERSION_MINOR = "1";
+	public static final String VERSION_MINOR = "2";
 
 	/**
 	 * The current buildnumber of the program. This is autmoaticly changed every
 	 * build: so do not touch this variable!
 	 */
-	public static final String BUILDNUMBER = "2";
+	public static final String BUILDNUMBER = "1";
 
 	/**
 	 * The build time. This is autmoaticly changed every build: so do not touch
 	 * this variable!
 	 */
-	public static final String BUILD_TIME = "2014-08-10 17:17:06";
+	public static final String BUILD_TIME = "2014-08-15 18:13:59";
 
 }
--- src/com/simpletasker/ui/CodePane.java
+++ src/com/simpletasker/ui/CodePane.java
@@ -11,17 +11,27 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JWindow;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
+import sun.net.www.protocol.gopher.GopherClient;
+
 import com.simpletasker.lang.Executor;
 import com.simpletasker.lang.commands.Command;
 
+import java.awt.Color;
+import java.awt.DefaultKeyboardFocusManager;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.KeyEventDispatcher;
 import java.awt.Point;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
@@ -35,7 +45,7 @@ public class CodePane extends JScrollPane implements DocumentListener,
 	private JTextArea codeArea;
 
 	private JWindow suggestionsFrame;
-	private JPanel suggestionsPanel;
+	private JPanel suggestionsPanel = new JPanel();
 
 	// the current caret position
 	private int caretDot = 0;
@@ -45,6 +55,23 @@ public class CodePane extends JScrollPane implements DocumentListener,
 
 	// tells if the code/program is chaning the text in the codeArea JTextArea.
 	private boolean codeIsChaningText = false;
+
+	// the height of a suggestion label.
+	private static final int suggestionHeight = 30;
+
+	// borders for the SuggestionLabel
+	private final Border redBorder = BorderFactory.createLineBorder(Color.red,
+			1);
+	private final Border blackBorder = BorderFactory.createLineBorder(
+			UIManager.getColor("Panel.background"), 1);
+
+	/**
+	 * The sellected suggestion. If value is -1 than there is no suggestion
+	 * selected.
+	 */
+	private int selectedSuggestion = 0;
+	private ArrayList<Suggestion> suggestions = new ArrayList<>();
+	
 	/**
 	 * Called when the user changes the content of the {@link #codeArea}
 	 */
@@ -56,41 +83,108 @@ public class CodePane extends JScrollPane implements DocumentListener,
 			codeIsChaningText = false;
 		}
 	};
-	private int selected = 0;
-	/**
-	 * The sellected suggestion. If value is -1 than there is no suggestion
-	 * selected.
-	 */
-	private int slectedSuggestion = -1;
-	private ArrayList<Suggestion> suggestions = new ArrayList<>();
+
+	private KeyEventDispatcher onButtonDown = new KeyEventDispatcher() {
+		boolean b = false;
+
+		@Override
+		public boolean dispatchKeyEvent(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+				b = !b;
+				if (b)
+					return true;
+				selectedSuggestion++;
+				if (selectedSuggestion >= suggestions.size())
+					selectedSuggestion = 0;
+				updateSelected();
+				return true;
+			}
+			return false;
+		}
+	};
+
+	private KeyEventDispatcher onButtonUp = new KeyEventDispatcher() {
+		boolean b = false;
+		
+		@Override
+		public boolean dispatchKeyEvent(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_UP) {
+				b = !b;
+				if (b)
+					return true;
+				selectedSuggestion--;
+				if (selectedSuggestion < 0)
+					selectedSuggestion = suggestions.size() - 1;
+				updateSelected();
+				return true;
+			}
+			return false;
+		}
+	};
+	
+	private KeyEventDispatcher onButtonEnter = new KeyEventDispatcher() {
+		boolean b = false;
+
+		@Override
+		public boolean dispatchKeyEvent(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+				if(!suggestionsIsVisable())
+					return false;
+				b = !b;
+				if (b)
+					return true;
+				if(!suggestionsIsVisable())
+					return false;
+				Suggestion selected = getSelectedSuggestion();
+				codeIsChaningText = true;
+				
+				String txt = codeArea.getText();
+				String before = "", after = "";
+				
+				try{
+					before = txt.substring(0, getWordBegin());
+				}catch(Exception iliketrains){}
+				
+				try{
+					after = txt.substring(getWordEnd(), txt.length()-1);
+				}catch(Exception iliketrains){}
+				
+				codeArea.setText(before + selected.getText() + after);
+				codeIsChaningText = false;
+				
+				removeDropdownMenu();
+				
+				return true;
+			}
+			return false;
+		}
+	};
 
 	public CodePane() {
 		lineNumbers = new JTextArea("1");
 		lineNumbers.setEditable(false);
-		this.setRowHeaderView(lineNumbers);
+		setRowHeaderView(lineNumbers);
 
 		codeArea = new JTextArea();
 		codeArea.setEditable(true);
-		this.setViewportView(codeArea);
+		setViewportView(codeArea);
 
 		codeArea.getDocument().addDocumentListener(this);
 		codeArea.addCaretListener(this);
 
 		suggestionsFrame = new JWindow();
-		suggestionsFrame.setBounds(50, 50, 50, 50);
 		suggestionsFrame.setVisible(false);
-
 		suggestionsFrame.setAlwaysOnTop(true);
-
-		// suggestionsFrame.getRootPane().setBorder(BorderFactory.create);
-		// suggestionsFrame.setUndecorated(true);
-
-		suggestionsPanel = new JPanel();
-		suggestionsPanel.setBorder(BorderFactory.createRaisedBevelBorder());
-		suggestionsPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-
 		suggestionsFrame.setContentPane(suggestionsPanel);
 
+		suggestionsPanel.setLayout(new GridBagLayout());
+
+		DefaultKeyboardFocusManager.getCurrentKeyboardFocusManager()
+				.addKeyEventDispatcher(onButtonDown);
+		DefaultKeyboardFocusManager.getCurrentKeyboardFocusManager()
+				.addKeyEventDispatcher(onButtonUp);
+		DefaultKeyboardFocusManager.getCurrentKeyboardFocusManager()
+				.addKeyEventDispatcher(onButtonEnter);
 	}
 
 	/**
@@ -100,8 +194,10 @@ public class CodePane extends JScrollPane implements DocumentListener,
 	 */
 	private void updateDropdownMenu() {
 		String word = getCurrentlyTypedWord();
-		if (word.equals(""))
+		if (word.equals("")) {
+			removeDropdownMenu();
 			return;
+		}
 		// give the argument word so we do not have to call
 		// getCurrentlytypedWord() again.
 		int sizeBefore = suggestions.size();
@@ -113,38 +209,54 @@ public class CodePane extends JScrollPane implements DocumentListener,
 		}
 		boolean sizeChanged = suggestions.size() != sizeBefore;
 		if (suggestionsIsVisable() && !sizeChanged) {
-			updateSelected();
+			//updateSelected();
 		} else {
 			suggestionsPanel.removeAll();
+			int y = 0;
 			for (Suggestion s : suggestions) {
-				suggestionsPanel.add(s);
+				GridBagConstraints c = new GridBagConstraints();
+				c.gridy = y;
+				c.anchor = GridBagConstraints.WEST;
+				suggestionsPanel.add(s, c);
+				y++;
 			}
 			suggestionsFrame.setVisible(true);
-			suggestionsFrame.setBounds(100, 100, 200, 30 * suggestions.size());
+			suggestionsFrame.pack();
 		}
+		updateSelected();
 	}
 
 	private void updateSelected() {
+		if(selectedSuggestion > suggestions.size()-1)
+			selectedSuggestion = suggestions.size()-1;
 		int i = 0;
 		for (Suggestion s : suggestions) {
-			if (i == selected)
-				s.isSelected = true;
+			if (i == selectedSuggestion)
+				s.setSelected(true);
 			else
-				s.isSelected = false;
+				s.setSelected(false);
 			s.revalidate();
 			i++;
 		}
-
+		suggestionsFrame.pack();
 	}
 
 	private boolean suggestionsIsVisable() {
 		return suggestionsFrame.isVisible();
 	}
-
+	
+	private Suggestion getSelectedSuggestion(){
+		for(Suggestion s : suggestions)
+			if(s.isSelected)
+				return s;
+		return null;
+	}
+	
 	private void removeDropdownMenu() {
 		suggestions.clear();
 		suggestionsPanel.removeAll();
 		suggestionsFrame.setVisible(false);
+		selectedSuggestion = 0;
 	}
 
 	private void updateSuggestions(String word) {
@@ -165,7 +277,8 @@ public class CodePane extends JScrollPane implements DocumentListener,
 	public String getCurrentlyTypedWord() {
 		if (caretDot <= 0)
 			return "";
-		return codeArea.getText().substring(getWordBegin(), getWordEnd()).trim();
+		return codeArea.getText().substring(getWordBegin(), getWordEnd())
+				.trim();
 	}
 
 	/**
@@ -235,15 +348,27 @@ public class CodePane extends JScrollPane implements DocumentListener,
 	// CLASSES
 	// .............................................
 
-	private class Suggestion extends JLabel {
+	public class Suggestion extends JLabel {
 
 		private static final long serialVersionUID = 3273864444932843439L;
 
-		public boolean isSelected = false;
-
 		public Suggestion(String name) {
 			super(name);
-			setPreferredSize(new Dimension(220, 30));
+			setBorder(blackBorder);
+		}
+
+		private boolean isSelected = false;
+
+		public void setSelected(boolean isSelected) {
+			this.isSelected = isSelected;
+			if (isSelected)
+				setBorder(redBorder);
+			else
+				setBorder(blackBorder);
+		}
+
+		public boolean isSelected() {
+			return isSelected;
 		}
 
 		@Override
